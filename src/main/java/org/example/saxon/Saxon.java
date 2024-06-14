7dd929d34b6eddcdc1c6a68d0915314bf01c7c75
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2024-06-14 21:22:08 +0200

    update vikings

--- src/main/java/org/example/saxon/Saxon.java
+++ src/main/java/org/example/saxon/Saxon.java
@@ -3,7 +3,33 @@ package org.example.saxon;
 import org.example.soldier.Soldier;
 
 public class Saxon extends Soldier {
+
+    private String msg;
+
     public Saxon(int health, int strength) {
         super(health, strength);
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    @Override
+    public void receiveDamage(int damage) {
+        super.receiveDamage(damage);
+        if (health>0){
+            setMsg("A Saxon has received"+damage+"points of damage");
+        } else {
+            setMsg("A Saxon has died in combat");
+        }
+    }
+
+    @Override
+    public int attack() {
+        return super.attack();
     }
 }
