afd84ff50a074da8547c4892c3d67566279956f5
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2024-06-14 21:38:48 +0200

    update vikings

--- src/main/java/org/example/war/War.java
+++ src/main/java/org/example/war/War.java
@@ -1,4 +1,27 @@
 package org.example.war;
 
+import org.example.saxon.Saxon;
+import org.example.vikins.Vikings;
+
+import java.util.ArrayList;
+
 public class War {
+    private ArrayList<> vikingArmy;
+    private ArrayList<> saxonArmy;
+
+    public ArrayList getVikingArmy() {
+        return vikingArmy;
+    }
+
+    public void setVikingArmy(ArrayList vikingArmy) {
+        this.vikingArmy = vikingArmy;
+    }
+
+    public ArrayList getSaxonArmy() {
+        return saxonArmy;
+    }
+
+    public void setSaxonArmy(ArrayList saxonArmy) {
+        this.saxonArmy = saxonArmy;
+    }
 }
