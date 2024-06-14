e4f84d58242eca90bc199248b93401a6355a4edc
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2024-06-14 20:38:32 +0200

    update vikings

--- src/main/java/org/example/soldier/Soldier.java
+++ src/main/java/org/example/soldier/Soldier.java
@@ -1,4 +1,19 @@
 package org.example.soldier;
 
 public class Soldier {
+    public int health;
+    public int strength;
+
+    public Soldier(int health, int strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+
+    public int  attack(){
+        return strength;
+    }
+
+    public void receiveDamage(int damage){
+        health = health - damage;
+    }
 }
--- src/main/java/org/example/vikins/Vikings.java
+++ src/main/java/org/example/vikins/Vikings.java
@@ -1,4 +1,40 @@
 package org.example.vikins;
 
-public class Vikings {
+import org.example.soldier.Soldier;
+
+public class Vikings extends Soldier {
+    String name;
+    private String msg;
+
+    public Vikings(String name, int health, int strength) {
+        super(health, strength);
+        this.name = name;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    @Override
+    public void receiveDamage(int damage) {
+        super.receiveDamage(damage);
+        if (health>0){
+            setMsg(name+"has received"+damage+"points of damage");
+        } else {
+            setMsg(name+"has died in combat");
+        }
+    }
+
+    public String battleCry(){
+        return "Odin Owns You All!";
+    }
+
+    @Override
+    public int attack() {
+        return super.attack();
+    }
 }
